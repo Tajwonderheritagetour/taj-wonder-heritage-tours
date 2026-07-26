39a9b5b196f17dbf7b15a944c015aea97826dd2c
Tajwonderheritagetour <tajwelcometours@gmail.com>
2026-07-26 20:02:30 +0530

    Test Organization schema

--- components/seo/OrganizationSchema.tsx
+++ components/seo/OrganizationSchema.tsx
@@ -1,43 +1,14 @@
 export default function OrganizationSchema() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "TravelAgency",
-    name: "Taj Wonder Heritage Tours",
-    url: "https://www.tajwonderheritagetours.com",
-   logo: "https://www.tajwonderheritagetours.com/images/logo.png",
-    image: "https://www.tajwonderheritagetours.com/images/og-image.jpg",
-    description:
-      "Taj Wonder Heritage Tours is a private India tour operator offering Taj Mahal tours, Golden Triangle tours, Rajasthan tours and customized travel experiences with local experts.",
-
-    telephone: "+91-7830716981",
-
-    email: "info@tajwonderheritagetours.com",
-
-    address: {
-      "@type": "PostalAddress",
-      streetAddress:
-        "Building No. 9, Deepika Vihar, Barauli Ahir, Shamsabad Road",
-      addressLocality: "Agra",
-      addressRegion: "Uttar Pradesh",
-      postalCode: "282001",
-      addressCountry: "IN",
-    },
-
-    areaServed: "India",
-
-    priceRange: "$$",
-
-  sameAs: [
-  "https://www.tripadvisor.com/YourProfile",
-  "https://www.viator.com/YourProfile"
-]
-  };
-
   return (
     <script
+      id="organization-schema-test"
       type="application/ld+json"
       dangerouslySetInnerHTML={{
-        __html: JSON.stringify(schema),
+        __html: JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          name: "Test Organization",
+        }),
       }}
     />
   );
