6fc4a20979c7da08d93417dd3a68a39bbce245e6
Tajwonderheritagetour <tajwelcometours@gmail.com>
2026-07-28 22:29:58 +0530

    Update sitemap to use www domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://tajwonderheritagetours.com";
+const baseUrl = "https://www.tajwonderheritagetours.com";
 
   const pages = [
     "",
